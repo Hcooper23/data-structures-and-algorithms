b7de1eb75129a636613bd917e1677529b4f909bd
Hayden James Cooper <haydencpr95@gmail.com>
2023-07-17 23:17:15 -0400

    Code Challenge 43

--- javascript/tree-breadth-first/tree-breadth-first.js
+++ javascript/tree-breadth-first/tree-breadth-first.js
@@ -1,47 +1,23 @@
 function breadthFirst(tree) {
-
   if (!tree) {
-
     return [];
-
   }
 
-
-
-  var result = [];
-
-  var queue = [];
-
-  queue.push(tree);
-
-
+  const result = [];
+  const queue = [tree];
 
   while (queue.length > 0) {
-
-    var node = queue.shift();
-
+    const node = queue.shift();
     result.push(node.value);
 
-
-
     if (node.left) {
-
       queue.push(node.left);
-
     }
-
-
 
     if (node.right) {
-
       queue.push(node.right);
-
     }
-
   }
 
-
-
   return result;
-
 }
